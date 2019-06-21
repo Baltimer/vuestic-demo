5e87be68b95b1e61df04cea3419ff72d06acf0cf
Luis Cifre <luis.cifre@ejuniper.com>
2019-06-21 16:14:47 +0200

    First commit

---
+++ src/components/admin/app-sidebar/AppSidebar.vue
@@ -0,0 +1,50 @@
+<template>
+  <vuestic-sidebar :hidden="isOpen">
+    <template slot="menu">
+      <sidebar-link
+        :to="{ name: 'dashboard' }">
+        <span slot="title">
+          <span
+            class="sidebar-menu-item-icon vuestic-icon vuestic-icon-dashboard"></span>
+          <span>{{ $t('menu.dashboard') }}</span>
+        </span>
+      </sidebar-link>
+      <sidebar-link-group>
+        <span slot="title">
+          <span
+            class="sidebar-menu-item-icon vuestic-icon vuestic-icon-statistics"></span>
+          <span>{{ $t('menu.statistics') }}</span>
+        </span>
+        <sidebar-link
+          :to="{ name: 'charts' }">
+          <span slot="title">
+            <span>{{ $t('menu.charts') }}</span>
+          </span>
+        </sidebar-link>
+      </sidebar-link-group>
+    </template>
+  </vuestic-sidebar>
+</template>
+
+<script>
+
+import VuesticSidebar from '../../../vuestic-theme/vuestic-components/vuestic-sidebar/VuesticSidebar'
+import SidebarLink from './components/SidebarLink'
+import SidebarLinkGroup from './components/SidebarLinkGroup'
+
+export default {
+  name: 'app-sidebar',
+  components: {
+    VuesticSidebar,
+    SidebarLink,
+    SidebarLinkGroup,
+  },
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,99 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import AppLayout from '../components/admin/AppLayout'
+import AuthLayout from '../components/auth/AuthLayout'
+import lazyLoading from './lazyLoading'
+
+Vue.use(Router)
+
+const demoRoutes = []
+if (process.env.NODE_ENV === 'development') {
+  const VueBook = require('vue-book')
+
+  demoRoutes.push(
+    VueBook.createRoute({
+      requireContext: require.context('./..', true, /.demo.vue$/),
+      path: '/demo',
+    }),
+    VueBook.createRoute({
+      requireContext: require.context('./../components', true, /.vue$/),
+      path: '/presentation',
+    }),
+  )
+
+  Vue.use(VueBook.VueBookComponents)
+}
+
+const EmptyParentComponent = {
+  template: '<router-view></router-view>',
+}
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    ...demoRoutes,
+    {
+      path: '*',
+      redirect: { name: 'dashboard' },
+    },
+    {
+      path: '/auth',
+      component: AuthLayout,
+      children: [
+        {
+          name: 'login',
+          path: 'login',
+          component: lazyLoading('auth/login/Login'),
+        },
+        {
+          name: 'signup',
+          path: 'signup',
+          component: lazyLoading('auth/signup/Signup'),
+        },
+        {
+          path: '',
+          redirect: { name: 'login' },
+        },
+      ],
+    },
+    {
+      path: '/404',
+      component: EmptyParentComponent,
+      children: [
+        {
+          name: 'not-found-advanced',
+          path: 'not-found-advanced',
+          component: lazyLoading('pages/404-pages/VuesticPageNotFoundSearch'),
+        },
+        {
+          name: 'not-found-simple',
+          path: 'not-found-simple',
+          component: lazyLoading('pages/404-pages/VuesticPageNotFoundSimple'),
+        },
+        {
+          name: 'not-found-custom',
+          path: 'not-found-custom',
+          component: lazyLoading('pages/404-pages/VuesticPageNotFoundCustom'),
+        },
+        {
+          name: 'not-found-large-text',
+          path: '/pages/not-found-large-text',
+          component: lazyLoading('pages/404-pages/VuesticPageNotFoundLargeText'),
+        },
+      ],
+    },
+    {
+      name: 'crm',
+      path: '/crm',
+      component: AppLayout,
+      children: [
+        {
+          name: 'dashboard',
+          path: 'dashboard',
+          component: lazyLoading('crm/Dashboard'),
+          default: true
+        }
+      ],
+    }
+  ],
+})
